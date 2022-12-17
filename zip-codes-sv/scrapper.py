3ccd49e8d892f665f47455588f8c86b891c48c6e
StanDoge <StanlyDoge@gmail.com>
2022-12-17 15:13:43 -0600

    departaments enumeration

---
+++ zip-codes-sv/scrapper.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class Departaments(Enum):
+    ahuchapan = "ah"
+    sonsonate = "so"
+    santa_ana = "sa"
+    cabanas = "ca"
+    chalatenango = "ch"
+    cuscatlan = "cu"
+    la_libertad = "li"
+    la_paz = "pa"
+    san_salvador = "ss"
+    san_vicente = "sv"
+    morazan = "mo"
+    san_miguel = "sm"
+    usulutan = "us"
+    la_union = "un"
+    
+
