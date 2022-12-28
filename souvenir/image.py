92c306295a400299d39955a6e14a061176c235a4
StanDoge <stanlydoge@gmail.com>
2022-12-27 21:12:27 -0600

    `feat:` class Image for new scraper

---
+++ souvenir/image.py
@@ -0,0 +1,25 @@
+from bing_image_urls import bing_image_urls
+
+class Image:
+	
+	def __init__(self, endpoint:str):
+		...
+	
+	def switcher(self, endopint:str):
+		switch={
+			"ah":"ahuchapan",
+			"so":"sonsonate",
+			"sa":"santa ana",
+			"ca":"cabanas",
+			"ch":"chalatenango",
+			"cu":"cuscatlan",
+			"li":"la libertad",
+			"pa":"la paz",
+			"ss":"san salvador",
+			"sv":"san vicente",
+			"mo":"morazan",
+			"sm":"san miguel",
+			"us":"usulutan",
+			"un":"la union"
+		}
+	
