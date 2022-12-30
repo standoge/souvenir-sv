8e9e165d19d88ac54981eec4af552c554e9d92a9
StanDoge <stanlydoge@gmail.com>
2022-12-29 19:12:34 -0600

    update imports in tests

--- tests/department_test.py
+++ tests/department_test.py
@@ -1,13 +1,15 @@
 import unittest
 
-from zipcode.department import Department, Endpoint
+from souvenir.zipcode import Endpoint, Zipcode, ImageBing, ImageGoogle
 
 
 class TestComponent(unittest.TestCase):
     def setUp(self):
         """Generate instance of Department to test it."""
         self.my_dep = Endpoint.san_vicente.value
-        self.my_object = Department(self.my_dep)
+        self.zip = Zipcode(self.my_dep)
+        self.image_bing = ImageBing(self.my_dep)
+        self.image_google = ImageGoogle(self.my_dep)
 
     def test_enumeration(self):
         """Test if Enum has 14 values regarded to 14 departments."""
@@ -19,11 +21,11 @@ class TestComponent(unittest.TestCase):
         Test if method returns None type, it mean that filter argument
         is wrong and bs4 doesn't find any match.
         """
-        self.assertNotEqual(self.my_object.summary, None)
+        self.assertNotEqual(self.zip.summary, None)
 
     def test_zipcodes(self):
         """
         Test if method returns None type, it mean that array with
         HTML labels match doesn't exist in filter argument used.
         """
-        self.assertNotEqual(self.my_object.zip_codes, None)
+        self.assertNotEqual(self.zip.zip_codes, None)
